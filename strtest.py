d85e06cc10109147f39c1204a706aeef79747df3
khkwon01 <khkwon01@gmail.com>
2025-05-26 18:30:31 +0900

    Update strtest.py

--- strtest.py
+++ strtest.py
@@ -7,6 +7,15 @@ def main():
 
   st.text("테스트 페이지입니다.")
 
+  html = """
+  <div style="background-color:blue:padding:10px;">
+  <h1 stype="color:yellow">테스트 페이지</h1>
+  """
+
+  st.markdown(html, unsafe_allow_html=True)
+  st.write("테스트 페이지...")
+
+  st.sidebar.image("", width=300)
 
 if __name__ == "__main__":
   main()

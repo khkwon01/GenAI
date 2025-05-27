2dd73b3bb834ab90224d501e5378c987c4cc8e8a
khkwon01 <khkwon01@gmail.com>
2025-05-27 13:04:34 +0900

    Update strtest.py

--- strtest.py
+++ strtest.py
@@ -5,7 +5,7 @@ def main():
   """ test... """
   st.title("Askme Web app")
 
-  st.text("테스트 페이지입니다.")
+  # st.text("테스트 페이지입니다.")
 
   html = """
   <div style="background-color:blue:padding:10px;">
@@ -14,7 +14,7 @@ def main():
   """
 
   st.markdown(html, unsafe_allow_html=True)
-  st.write("테스트 페이지...")
+  # st.write("테스트 페이지...")
 
   st.sidebar.image("assets/hw.png", width=300)
   img = st.sidebar.file_uploader("파일 올려주세요. (jpg, png or jpeg)", type=["jpg", "png", "jpeg"])
@@ -30,6 +30,7 @@ def main():
 
   if choice == "Guide":
     st.subheader("Guide for migration")
+    migtype = st.sidebar.radio("데이터베이스", ["MySQL", "AWS RDS", "AWS Mariadb"])
   elif choice == "Q&A":
     st.subheader("Q&A")
   elif choice == "etc":
